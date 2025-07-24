8099ccecb2fe464bad4c397af6c61151d3d11a33
jiya sohal <jiya.sohal.yass@gmail.com>
2025-07-24 09:46:07 -0400

    Initial Commit

---
+++ NavigationPrac/NavigationPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationPracApp.swift
+//  NavigationPrac
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
